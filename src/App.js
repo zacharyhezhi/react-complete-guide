2a998552703be19923bbf6626713dead6592e221
Zachary <hezhibruno@gmail.com>
2020-05-06 12:33:56 +1000

    work with props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a react App</h1>
         <p>Hello to my money job</p>
-        <Person />
+        <Person name = "Zachary" age = "28"/>
+        <Person name = "Max" age = "25">My hobby: Racing</Person>
+        <Person name = "Jeni" age = "24"/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 
-const person = () => {
-return <p>I'm a person and I am {Math.floor(Math.random() * 30)} years old</p>
+const person = (props) => {
+return <p>I'm {props.name} and I am {props.age} years old</p>
 }
 
 export default person;
