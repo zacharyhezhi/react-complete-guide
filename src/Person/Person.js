217a7155ab93f19c5f19b288eba34cc35de589d6
Zachary <hezhibruno@gmail.com>
2020-05-06 12:18:11 +1000

    Output dynamic component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const person = () => {
-    return <p>I'm a person test</p>
+return <p>I'm a person and I am {Math.floor(Math.random() * 30)} years old</p>
 }
 
 export default person;
