6dfe80267fc5a17c565893e48ccb054b4cf59b3c
Zachary <hezhibruno@gmail.com>
2020-05-13 20:52:27 +1000

    ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import styled from 'styled-components';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 
 const StyledButton = styled.button`
@@ -85,12 +86,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key = {person.id}>
+              <Person
               click = {() => this.detelePersonsHandler(index)}
               name = {person.name}
               age = {person.age}
               key = {person.id}
               changed = {(event) => this.nameChangeHandler(event, person.id)}/>
+              </ErrorBoundary>
           })}
         </div>
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -16,6 +16,11 @@ const StyledDiv = styled.div`
     }`
 
 const person = (props) => {
+
+    const rnd = Math.random();
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }
 return (
     <StyledDiv>
         <p onClick = {props.click}>I'm {props.name} and I am {props.age} years old</p>
